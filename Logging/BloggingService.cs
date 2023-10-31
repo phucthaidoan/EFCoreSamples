fd4a570cd86fb53254af888e7a591caeb57d6830
Phuc Thai <bindirt@gmail.com>
2023-10-31 18:58:45 +0700

    Added logging sample.

---
+++ Logging/BloggingService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Logging;
+
+public class BloggingService : IBloggingService
+{
+    private readonly BloggingContext _dbContext;
+    private readonly ILogger<BloggingService> _logger;
+
+    public BloggingService(
+        BloggingContext dbContext,
+        ILogger<BloggingService> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public void GetWithoutQueryTags()
+    {
+        var _ = _dbContext.Blogs.Where(x => x.BlogId == 5).ToList();
+    }
+
+    public void GetWithQueryTags()
+    {
+        var _ = _dbContext
+            .Blogs
+            .TagWith("GetBlogs")
+            .ToList();
+    }
+
+    public void GetWithExtraLogs()
+    {
+        using (_logger.BeginScope(new Dictionary<string, object> { { "EFQueries", "GetBlogs" } }))
+        {
+            var _ = _dbContext
+                .Blogs
+                .TagWith("GetBlogs")
+                .ToList();
+        }
+    }
+
+    public void InsertWithoutLogScope()
+    {
+        var blogging = new Blog
+        {
+            Url = "fake_url"
+        };
+        _dbContext.Add(blogging);
+        _dbContext.SaveChanges();
+    }
+
+    public void InsertWithLogScope()
+    {
+        var blogging = new Blog
+        {
+            Url = "fake_url"
+        };
+        _dbContext.Add(blogging);
+        using (_logger.BeginScope(new Dictionary<string, object> { { "EFQueries", "InsertBlog" } }))
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+
+    public void ExecuteRawQueryWithoutQueryTags()
+    {
+        _dbContext.Blogs
+            .FromSql($"SELECT * FROM Blogs")
+            .ToList();
+    }
+
+    public void ExecuteRawQueryWithQueryTags()
+    {
+        using (_logger.BeginScope(new Dictionary<string, object> { { "EFQueries", "FromSql_AllBlog" } }))
+        {
+            _dbContext.Blogs
+                .FromSql($"SELECT * FROM Blogs")
+                .ToList();
+        }
+    }
+}
+
+public interface IBloggingService
+{
+    void GetWithoutQueryTags();
+    void GetWithQueryTags();
+    void GetWithExtraLogs();
+    void InsertWithoutLogScope();
+    void InsertWithLogScope();
+    void ExecuteRawQueryWithoutQueryTags();
+    void ExecuteRawQueryWithQueryTags();
+}
---
+++ Logging/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using SeriLogThemesLibrary;
+
+var builder = Host
+    .CreateDefaultBuilder(args)
+    .UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+        .Enrich.FromLogContext()
+        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+        .WriteTo.Seq("http://localhost:5341")
+        .WriteTo.Console(theme: SeriLogCustomThemes.Theme2())
+    )
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<BloggingContext>();
+        services.AddScoped<IBloggingService, BloggingService>();
+    });
+
+using IHost host = builder.Build();
+var bloggingService = host.Services.GetService<IBloggingService>();
+// uncomment to give a try.
+
+// bloggingService.GetWithQueryTags();
+// bloggingService.GetWithExtraLogs();
+// bloggingService.InsertWithoutLogScope();
+// bloggingService.InsertWithLogScope();
+// bloggingService.ExecuteRawQueryWithoutQueryTags();
+bloggingService.ExecuteRawQueryWithQueryTags();
+
+await host.RunAsync();
+
+
