9c41ec86ff5b2055228e98c412100c5d53ce0917
Phuc Thai <bindirt@gmail.com>
2023-11-25 16:00:59 +0700

    Added sample of backing field working as computed field

---
+++ Modeling/BackingFields.ComputedField.EF8/Model.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BackingFields.EF8.Simple
+{
+    public class PersonContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => builder.AddConsole());
+        public string DbPath { get; }
+
+        public DbSet<Person> People { get; set; }
+
+        public PersonContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .Property(p => p.IsOver18)
+                .HasField("_isOver18");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options
+            .UseLoggerFactory(MyLoggerFactory) // Enable EF Core logging
+            .UseSqlServer(
+                "Server=LAPTOP-IAJ1J0A2;Database=backingfield_computed;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+    }
+
+    public class Person
+    {
+        private bool _isOver18;
+        private int _age;
+
+        public bool IsOver18 { get; private set; }
+
+        public int Id { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                _age = value;
+                if (_age > 18)
+                {
+                    _isOver18 = true;
+                }
+                else
+                {
+                    _isOver18 = false;
+                }
+            }
+        }
+    }
+}
---
+++ Modeling/BackingFields.ComputedField.EF8/Program.cs
@@ -0,0 +1,32 @@
+﻿using BackingFields.EF8.Simple;
+
+using (var dbContext = new PersonContext())
+{
+    await dbContext.Database.EnsureDeletedAsync();
+    await dbContext.Database.EnsureCreatedAsync();
+
+    await dbContext.AddRangeAsync(new List<Person>
+        {
+            new Person
+            {
+                Age = 9,
+            },
+            new Person
+            {
+                Age = 49,
+            }
+        });
+    await dbContext.SaveChangesAsync();
+}
+
+using (var dbContext = new PersonContext())
+{
+    dbContext.People.Select(p => new
+    {
+        p.Id,
+        p.Age,
+        p.IsOver18
+    })
+    .ToList()
+    .ForEach(p => Console.WriteLine($"{p.Age} - {p.IsOver18}"));
+}
